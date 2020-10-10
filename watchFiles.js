f6474d3c0399433b4ae8ea95ad6326db097e325c
James Rollings <James.Rollings@Civica.co.uk>
2020-10-10 15:12:18 +0100

    added more mappings, other additions

--- watchFiles.js
+++ watchFiles.js
@@ -1,21 +1,40 @@
+require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const chokidar = require('chokidar');
 
-const downloadsFolder = 'C:\\Users\\James.Rollings\\Downloads'
+const folderName = 'Downloads'
+const basePath = path.resolve(process.env.HOMEDRIVE, process.env.HOMEPATH)
+const folderToWatch = path.resolve(basePath, folderName)
 
 const extensions = {
     'Pictures': ['.png','.jpg','.jpeg'],
-    'Documents': ['.docx', 'doc', 'xlsx']
+    'Documents\\Word': ['.docx', '.doc'],
+    'Documents\\PDF': ['.pdf'],
+    'Documents\\Excel': ['.xlsx', '.xls'],
+    'Documents\\CSV': ['.csv'],
+    'Documents\\Text': ['.txt'],
+    'Documents\\JSON': ['.json'],
+    'Documents\\YAML': ['.yml', '.yaml'],
+    'Documents\\Zipped': ['.zip', '.7z'],
+    'Documents\\Installation': ['.exe', '.msi'],
+    'Documents\\Javascript': ['.js'],
+    'Documents\\Python': ['.py']
 }
-
+// Doesn't handle files inside folders yet
 const moveFiles = (filePath) => {
     const fileExtension = path.extname(filePath);
     const newDirectory = Object.keys(extensions).find(key => extensions[key].includes(fileExtension))
     if (newDirectory) {
-        const newPath = filePath.replace('Downloads', newDirectory);
+        const pathToCheck = path.resolve(basePath, newDirectory)
+        if (newDirectory.includes('\\') && !fs.existsSync(pathToCheck)) {
+            fs.mkdirSync(pathToCheck)
+        }
+        const newPath = filePath.replace(folderName, newDirectory);
         fs.renameSync(filePath, newPath)
+    } else {
+        console.log(`${path.basename(filePath)} hasn't been moved as no mapping exists.`)
     }
 }
 
-chokidar.watch(downloadsFolder).on('add', strPath => moveFiles(strPath));
+chokidar.watch(folderToWatch).on('add', strPath => moveFiles(strPath));
