29f8af166475403638966362fb096d00c54de533
James Rollings <james.rollings@civica.co.uk>
2022-07-04 16:44:49 +0100

    switch to typescript, bring repo up to date

--- src/watchFiles.ts
+++ src/watchFiles.ts
@@ -1,14 +1,16 @@
-require('dotenv').config();
-const fs = require('fs');
-const path = require('path');
-const chokidar = require('chokidar');
-const chalk = require('chalk');
+import dotenv from 'dotenv';
+import fs from 'fs-extra';
+import path from 'path';
+import chokidar from 'chokidar';
+import chalk from 'chalk';
+
+dotenv.config();
 
 const folderName = 'Downloads';
-const basePath = path.resolve(process.env.HOMEDRIVE, process.env.HOMEPATH);
-const folderToWatch = path.resolve(basePath, folderName);
+const basePath: string = path.resolve(process.env.HOMEDRIVE!, process.env.HOMEPATH!);
+const folderToWatch: string = path.resolve(basePath, folderName);
 
-const extensions = {
+const extensions: {[path: string] : string[]} = {
   'Pictures': ['.png', '.jpg', '.jpeg'],
   [`Documents${path.sep}Word`]: ['.docx', '.doc'],
   [`Documents${path.sep}PDF`]: ['.pdf'],
@@ -25,29 +27,47 @@ const extensions = {
   [`Documents${path.sep}HTMLCSS`]: ['.html', '.css'],
 };
 
-const moveFiles = (filePath) => {
+function extensionHasMapping(extension: string): boolean {
+  const arrExtensions: string[] = Object.values(extensions).flat();
+  return arrExtensions.includes(extension);
+}
+
+async function moveFiles(filePath: string) {
+
+  const fileExtension = path.extname(filePath);
+
+  if (fileExtension.length === 0 || !extensionHasMapping(fileExtension)) {
+    console.log(`${chalk.greenBright(path.basename(filePath))} hasn't been moved as no mapping exists for ${chalk.magentaBright(fileExtension)} files.`);
+    return;
+  }
+
   const pathWithoutBaseName = filePath.split(path.sep).slice(0, -1).join(path.sep);
+
   if (pathWithoutBaseName !== folderToWatch) {
     console.log(`${chalk.greenBright(path.basename(filePath))} exists in subdirectory ${chalk.redBright(pathWithoutBaseName.replace(basePath, '').replace(`${path.sep}${folderName}${path.sep}`, ''))} so won't be moved`);
     return;
   }
-  const fileExtension = path.extname(filePath);
+
   const newDirectory = Object.keys(extensions).filter((key) => extensions[key].includes(fileExtension.toLowerCase()));
+
   if (newDirectory.length) {
     if (newDirectory.length !== 1) {
       console.log(`Multiple mappings found for ${chalk.magentaBright(fileExtension)} files, no files of this type will be moved.`);
       return;
     }
+  
     const pathToCheck = path.resolve(basePath, newDirectory[0]);
-    if (newDirectory[0].includes(path.sep) && !fs.existsSync(pathToCheck)) {
-      fs.mkdirSync(pathToCheck);
+    const pathDoesExist = await fs.pathExists(pathToCheck)
+  
+    if (newDirectory[0].includes(path.sep) && !pathDoesExist) {
+      await fs.mkdir(pathToCheck);
     }
+  
     const newPath = filePath.replace(folderName, newDirectory[0]);
-    fs.renameSync(filePath, newPath);
-    console.log(`Moved ${chalk.greenBright(path.basename(filePath))} to ${chalk.cyanBright(newPath.replace(basePath, '').substring(1))}`);
-  } else {
-    console.log(`${chalk.greenBright(path.basename(filePath))} hasn't been moved as no mapping exists for ${chalk.magentaBright(fileExtension)} files.`);
-  }
-};
+    await fs.rename(filePath, newPath);
 
-chokidar.watch(folderToWatch, { awaitWriteFinish: true, ignorePermissionErrors: true }).on('add', (strPath) => moveFiles(strPath));
+    console.log(`Moved ${chalk.greenBright(path.basename(filePath))} to ${chalk.cyanBright(newPath.replace(basePath, '').substring(1))}`);
+  }
+}
+
+chokidar.watch(folderToWatch, { awaitWriteFinish: true, ignorePermissionErrors: true }).on('add', async (strPath) => await moveFiles(strPath));
